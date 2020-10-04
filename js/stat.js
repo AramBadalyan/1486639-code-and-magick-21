3e0d940f884178a3d84472a7dccc0b19e89f2dc4
AramBadalyan <Arampitek@gmail.com>
2020-10-04 14:07:23 +0300

    ESLint error fixes

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var FONT_FAMILY = 'PT Mono';
+var FONT_FAMILY = `PT Mono`;
 var FONT_SIZE = 16;
 
 var BAR_HEIGHT = 150;
@@ -19,7 +19,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var renderText = function (ctx, x, y, textToRender) {
-  var textLines = textToRender.split('\n');
+  var textLines = textToRender.split(`\n`);
 
   for (var i = 0; i < textLines.length; i++) {
     ctx.fillText(textLines[i], x, y + (FONT_SIZE + GAP) * i);
@@ -40,30 +40,30 @@ var getMaxValue = function (array) {
 
 var getRandomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
-}
+};
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(
       ctx,
       CLOUD_X + GAP,
       CLOUD_Y + GAP,
-      'rgba(0, 0, 0, 0.7)'
+      `rgba(0, 0, 0, 0.7)`
   );
   renderCloud(
       ctx,
       CLOUD_X,
       CLOUD_Y,
-      '#fff'
+      `#fff`
   );
 
   ctx.font = `bold ${FONT_SIZE}px ${FONT_FAMILY}`;
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000';
+  ctx.textBaseline = `hanging`;
+  ctx.fillStyle = `#000`;
   renderText(
       ctx,
       CLOUD_X + GAP * 2,
       CLOUD_Y + GAP * 2,
-      'Ура вы победили! \nСписок результатов:'
+      `Ура вы победили! \nСписок результатов:`
   );
 
   var maxTime = getMaxValue(times);
@@ -71,7 +71,7 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     var currentBarWidth = (BAR_HEIGHT * times[i]) / maxTime;
     var timeRounded = Math.round(times[i]);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = `#000`;
 
     ctx.fillText(
         names[i],
@@ -85,8 +85,8 @@ window.renderStatistics = function (ctx, names, times) {
         CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_SIZE - GAP - currentBarWidth - FONT_SIZE
     );
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] === `Вы`) {
+      ctx.fillStyle = `rgba(255, 0, 0, 1)`;
     } else {
       ctx.fillStyle = `hsl(240, ${getRandomInt(100)}%, 50%)`;
     }
